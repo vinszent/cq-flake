0609cdd0f22fe08ff2df1d8f6442070e3dfc04ef
Marcus Boyd <mwb@geosol.com.au>
2021-06-17 10:09:48 +0930

    OCP: dump symbols step added

---
+++ expressions/OCP/dump_symbols.py
@@ -0,0 +1,18 @@
+import lief
+import sys
+from path import Path
+
+prefix = sys.argv[1]
+
+prefix_linux = Path(prefix).expand()
+libs = prefix_linux.glob('**/libTK*.so.7.5.1')
+
+exported_symbols = []
+
+for lib in libs:
+    p = lief.parse(lib)
+    for s in p.exported_symbols:
+        exported_symbols.append(f'{s.name}\n')
+        
+with open(f'symbols_mangled_linux.dat','w') as f:
+    f.writelines(exported_symbols)
