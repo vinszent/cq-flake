9494c07651ee094f379fb9a5fc8a9910bac222ae
Ethan Rooke <ethan@roo.ke>
2024-10-02 16:42:09 -0500

    add spyder v5 expression
    cq-editor requires spyder major version 5, nixpkgs has moved on to 6.

--- expressions/py-overrides.nix
+++ expressions/py-overrides.nix
@@ -44,4 +44,8 @@
   cq-warehouse = self.callPackage ./cq-warehouse.nix { };
 
   qtconsole = self.callPackage ./qtconsole.nix {};
+
+  spyder-kernels = self.callPackage ./spyder-kernels.nix {};
+
+  spyder = self.callPackage ./spyder {};
 }
